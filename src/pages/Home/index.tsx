05dd986c3550d1cbed525672b9a980018f0fb17a
felipesanderp <felipe.panisson@hotmail.com>
2023-11-17 16:05:27 -0300

    initial layut to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,26 @@
-import { HomeContainer } from './styles'
+import { HomeContainer, LogoContainer, Logo } from './styles'
+
+import logoImg from '../../assets/logo-findafriend.svg'
+import dogsImg from '../../assets/dogs.png'
 
 export function Home() {
-  return <HomeContainer></HomeContainer>
+  return (
+    <HomeContainer>
+      <LogoContainer>
+        <Logo>
+          <img src={logoImg} alt="FindAFriend" />
+          <h2>FindAFriend</h2>
+        </Logo>
+
+        <h1>Leve a felicidade para o seu lar</h1>
+
+        <span>
+          Encontre o animal de estimação ideal para o seu estilo de vida!
+        </span>
+      </LogoContainer>
+      <div>
+        <img src={dogsImg} alt="" />
+      </div>
+    </HomeContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,47 @@
 import { styled } from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  padding-left: 15rem;
+
+  grid-template-columns: 500px 1fr;
+
+  background-color: ${(props) => props.theme['red-500']};
+`
+
+export const LogoContainer = styled.div`
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 700;
+    margin-bottom: 5rem;
+
+    color: ${(props) => props.theme.white};
+  }
+
+  span {
+    font-size: 1.5rem;
+    font-weight: 600;
+
+    color: ${(props) => props.theme.white};
+  }
+`
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  margin-bottom: 5rem;
+
+  h2 {
+    color: ${(props) => props.theme.white};
+  }
+`
