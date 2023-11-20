adaf65c3a33d80f612139600abb173122c3043ee
felipesanderp <felipe.panisson@hotmail.com>
2023-11-20 16:04:47 -0300

    initial login page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
+import { LoginPage } from './pages/(Admin)/Login'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="/admin">
+        <Route path="login" element={<LoginPage />} />
+      </Route>
     </Routes>
   )
 }
